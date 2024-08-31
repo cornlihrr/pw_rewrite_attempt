c015f1a1b848c65af8d84728753323998c853359
Khristian Tan <89771718+Loadsofskeptic@users.noreply.github.com>
2024-08-31 15:13:46 +0800

    Update scriptlol.js

--- scriptlol.js
+++ scriptlol.js
@@ -15,7 +15,7 @@ function normal(x)
 function abnormal(y)
 {
     y.className = "normalsize";
-    imglol.src = "https://media.discordapp.net/attachments/1276523594364616817/1278189967213854831/image.png?ex=66d289ab&is=66d1382b&hm=d673f4d06844a148034cbbcb5cda90fbb0a15e385a3ec34d6ccefb218f3616f3&=&";
+    imglol.src = "images/entitledpiece";
     y.classList.remove("smolsize");
     imglol.style = 'transition: width 0.2s';
 }
